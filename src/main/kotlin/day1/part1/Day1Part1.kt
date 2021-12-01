9394129937cfe8cb100a1394dac23f70d9a91797
g0rd1 <gordigev@gmail.com>
2021-12-01 22:44:29 +0500

    Day 1

---
+++ src/main/kotlin/day1/part1/Day1Part1.kt
@@ -0,0 +1,10 @@
+import java.io.File
+
+fun main() {
+    val path = "src\\main\\kotlin\\day1\\part1\\input.txt"
+    val measurements = File(path).readLines().map { it.toInt() }
+    val answer = (1 until measurements.count()).reduce { acc, index ->
+        if (measurements[index] > measurements[index - 1]) acc + 1 else acc
+    }
+    println(answer)
+}
---
+++ src/main/kotlin/day1/part2/Day1Part2.kt
@@ -0,0 +1,12 @@
+import java.io.File
+
+fun main() {
+    val path = "src\\main\\kotlin\\day1\\part1\\input.txt"
+    val measurements = File(path).readLines().map { it.toInt() }
+    val windowedMeasurements = measurements.windowed(3)
+    val sumOfWindowedMeasurements = windowedMeasurements.map { it.sum() }
+    val answer = (1 until sumOfWindowedMeasurements.count()).reduce { acc, index ->
+        if (sumOfWindowedMeasurements[index] > sumOfWindowedMeasurements[index - 1]) acc + 1 else acc
+    }
+    println(answer)
+}
