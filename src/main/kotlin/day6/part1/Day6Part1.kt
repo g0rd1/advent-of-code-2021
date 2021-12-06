49fe529e0f7371d5dab9b9ee049299b0065a540d
g0rd1 <gordigev@gmail.com>
2021-12-06 21:27:59 +0500

    Day 6

---
+++ src/main/kotlin/day6/part1/Day6Part1.kt
@@ -0,0 +1,27 @@
+import java.io.File
+
+fun main() {
+    val path = "src\\main\\kotlin\\day6\\input.txt"
+    val input = File(path).readText()
+    val lanternfishTimerToCount = input
+        .split(",")
+        .map { it.trim().toInt() }
+        .groupingBy { it }
+        .eachCount()
+    println(getAfterDays(lanternfishTimerToCount, 80).values.sum())
+}
+
+private fun getAfterDays(lanternfishTimerToCount: Map<Int, Int>, days: Int): Map<Int, Int> {
+    if (days < 1) return lanternfishTimerToCount
+    return getAfterDays(getAfterDay(lanternfishTimerToCount), days - 1)
+}
+
+private fun getAfterDay(lanternfishTimerToCount: Map<Int, Int>): Map<Int, Int> {
+    return (0..8).associateWith {
+        when (it) {
+            8 -> lanternfishTimerToCount[0] ?: 0
+            6 -> (lanternfishTimerToCount[it + 1] ?: 0) + (lanternfishTimerToCount[0] ?: 0)
+            else -> lanternfishTimerToCount[it + 1] ?: 0
+        }
+    }
+}
---
+++ src/main/kotlin/day6/part2/Day6Part2.kt
@@ -0,0 +1,28 @@
+import java.io.File
+
+fun main() {
+    val path = "src\\main\\kotlin\\day6\\input.txt"
+    val input = File(path).readText()
+    val lanternfishTimerToCount = input
+        .split(",")
+        .map { it.trim().toInt() }
+        .groupingBy { it }
+        .eachCount()
+        .mapValues { (_, count) -> count.toLong() }
+    println(getAfterDays(lanternfishTimerToCount, 256).values.sum())
+}
+
+private fun getAfterDays(lanternfishTimerToCount: Map<Int, Long>, days: Int): Map<Int, Long> {
+    if (days < 1) return lanternfishTimerToCount
+    return getAfterDays(getAfterDay(lanternfishTimerToCount), days - 1)
+}
+
+private fun getAfterDay(lanternfishTimerToCount: Map<Int, Long>): Map<Int, Long> {
+    return (0..8).associateWith {
+        when (it) {
+            8 -> lanternfishTimerToCount[0] ?: 0
+            6 -> (lanternfishTimerToCount[it + 1] ?: 0) + (lanternfishTimerToCount[0] ?: 0)
+            else -> lanternfishTimerToCount[it + 1] ?: 0
+        }
+    }
+}
